3623aff6605efa042872c531f39138760a5a81a6
Vitaliy Shagin <redwind090@gmail.com>
2025-10-06 17:26:15 +0300

    init project

---
+++ migrations/20251006142432_init.down.sql
@@ -0,0 +1,6 @@
+-- Add down migration script here
+DROP TABLE IF EXISTS "request";
+DROP TABLE IF EXISTS "employee_specs";
+DROP TABLE IF EXISTS "employee";
+DROP TABLE IF EXISTS "service";
+DROP TABLE IF EXISTS "user";
