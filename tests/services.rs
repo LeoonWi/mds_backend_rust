1df934b03ef7c8ffb8a0bcf9f03a0c0afe4b0a25
Vitaliy Shagin <redwind090@gmail.com>
2025-10-10 17:16:35 +0300

    update integration test for output pretty json result

--- tests/services.rs
+++ tests/services.rs
@@ -4,11 +4,18 @@ use sqlx::PgPool;
 
 #[sqlx::test(migrations = "./migrations")]
 async fn test_create_service(pool: PgPool) {
+    eprintln!("Testing create service");
     let app = features::services::new(&pool);
     let server = axum_test::TestServer::new(app).unwrap();
     let payload = models::dto::Service::new(None, Some("Создание сайта".to_string()));
 
     let response = server.post("/create_service").json(&payload).await;
+    let json_body: models::dto::Service = response.json();
+    eprintln!(
+        "Result first request:\n{}",
+        serde_json::to_string_pretty(&json_body).expect("Failed to format JSON")
+    );
+
     assert_eq!(response.status_code(), StatusCode::OK);
 
     let response = server.post("/create_service").json(&payload).await;
